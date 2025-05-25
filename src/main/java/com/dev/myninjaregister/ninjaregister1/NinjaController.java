d424acebc225df32f43faf1e120bc71288358734
rafaelonias <rafasecund97@gmail.com>
2025-05-25 15:44:55 -0300

    Ninja regsiter entity

--- src/main/java/com/dev/myninjaregister/ninjaregister1/NinjaController.java
+++ src/main/java/com/dev/myninjaregister/ninjaregister1/NinjaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
     @GetMapping("/wellcome")
     public String wellcome(){
         return "Wellcome in my first spring page!";
